707a5088373c50e5f18662dbd409ca3746852da5
Deivid Souza <ddeividd.eu@hotmail.com>
2020-12-04 10:34:14 -0300

    Iniciando aplicaçao ToDos

---
+++ java_script/3) App de ToDos/2)iniciando_aplicacao.js
@@ -0,0 +1,9 @@
+var listElement  = document.querySelector('#app ul');
+var inputElment  = document.querySelector('#app input');
+var buttonElment = document.querySelector('#app button');
+
+var todos = [
+    'Fazer Café',
+    'Estudar JS',
+    'Participar da comunidade da Rocketseat'
+];
